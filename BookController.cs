eac8c57f0ca6ab8ce53bec061046f256f4495a80
ALiHammudah <156255159+ALiHammudah@users.noreply.github.com>
2024-01-31 23:31:01 +0200

    Add files via upload

---
+++ BookController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
+using RepostriyUOW.core.Models;
+using RepostriyUOW.core.Repostries;
+
+namespace RepostriyUOW.Controller
+{
+    [ApiController]
+    [Route("Controller")]
+    public class BookController : ControllerBase
+    {
+        protected readonly IBaseRepostry<Book> _bookRepostry;
+
+        public BookController(IBaseRepostry<Book> bookRepostry)
+        {
+            _bookRepostry = bookRepostry;
+        }
+
+        [HttpGet("get_book_by_id")]
+        public Book GetByID()
+        {
+            return _bookRepostry.GetById(2);
+        }
+    }
+}
