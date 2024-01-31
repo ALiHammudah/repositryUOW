e3a171dce9c2eeaa3a6feaef9ce807445fd91b00
ALiHammudah <156255159+ALiHammudah@users.noreply.github.com>
2024-01-31 23:30:06 +0200

    Add files via upload

---
+++ RepostriyUOW.EF/ApplicationDbContext.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Microsoft.Extensions.Options;
+using RepostriyUOW.core.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RepostriyUOW.EF
+{
+    public class ApplicationDbContext : DbContext
+    {
+
+        //public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        //{
+        //}
+
+        public DbSet<Book> books { get; set; }
+        public DbSet<Author> authors { get; set; }
+        public DbSet<Customer> customers { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=DESKTOP-FAJC3VE\\SQLEXPRESS;Database=ApiDemo;" +
+                "Trusted_Connection=True;trustservercertificate=true");
+
+            base.OnConfiguring(optionsBuilder);
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+
+        public class bookConfig : IEntityTypeConfiguration<Book>
+        {
+            public void Configure(EntityTypeBuilder<Book> builder)
+            {
+
+            }
+        }
+
+        public class customerConfig : IEntityTypeConfiguration<Customer>
+        {
+            public void Configure(EntityTypeBuilder<Customer> builder)
+            {
+
+            }
+        }
+
+        public class authorConfig : IEntityTypeConfiguration<Author>
+        {
+            public void Configure(EntityTypeBuilder<Author> builder)
+            {
+
+            }
+        }
+    }
+}
---
+++ RepostriyUOW.EF/Repostries/BaseRepostry.cs
@@ -0,0 +1,22 @@
+﻿using RepostriyUOW.core.Repostries;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RepostriyUOW.EF.Repostries
+{
+    public class BaseRepostry<T> : IBaseRepostry<T> where T : class
+    {
+        protected ApplicationDbContext _context;
+        public BaseRepostry(ApplicationDbContext context) => _context = context;
+
+        public T GetById(int id)
+        {
+            _context.Database.EnsureCreated();
+           return _context.Set<T>().Find(id);
+        }
+    }
+}
